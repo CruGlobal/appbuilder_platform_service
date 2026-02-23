612f1c5816fef3e30523b02b4c10c58a22150730
Wirachot <wirachot@gmail.com>
2026-02-23 14:06:00 +0700

    Fix onCreateAjv not define.

--- platform/ABModel.js
+++ platform/ABModel.js
@@ -901,6 +901,7 @@ module.exports = class ABModel extends ABModelCore {
             // to avoid Ajv strict mode warning: "use allowUnionTypes to allow union type keyword"
             static createValidator() {
                return new AjvValidator({
+                  onCreateAjv: (ajv) => {},
                   options: {
                      allErrors: true,
                      validateSchema: false,
